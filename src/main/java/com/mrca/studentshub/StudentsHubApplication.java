61feb45f768f52ea92c37a8e8256dc51b9b351d0
Mohan7989 <neelamohannadupuru@gmail.com>
2025-10-26 12:09:22 +0530

    updated

--- src/main/java/com/mrca/studentshub/StudentsHubApplication.java
+++ src/main/java/com/mrca/studentshub/StudentsHubApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MRCAStudentsHubApplication {
+public class StudentsHubApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MRCAStudentsHubApplication.class, args);
+        SpringApplication.run(StudentsHubApplication.class, args);
     }
 }
